0e108cbed0ddb0e6aba93c534f2934421fefb7f3
Lluís Rodríguez Mateu <1999lluis@gmail.com>
2018-02-15 13:14:11 +0100

    Big boi

---
+++ AA1.1/Enemigos.cpp
@@ -0,0 +1,35 @@
+#include <ctime>
+#include <string>
+#include <iostream>
+using namespace std;
+
+string enemyName[5] = { "Alucard", "Mallus", "Albedo", "Azzer", "Morg" };
+
+enum EnemyType { zombie, vampire, ghost, witch }; //ejercicio 1
+
+struct Enemy //ejercicio 2
+{
+	EnemyType type;
+	string name;
+	int health;
+};
+
+bool equalEnemies(Enemy e1, Enemy e2) //ejercicio 3
+{
+	return (e1.type == e2.type && e1.name == e2.name);
+}
+
+Enemy createRandomEnemy() //ejercicio 4
+{
+	Enemy e1;
+	e1.health = rand() % 500 + 1;
+	e1.name = enemyName[rand() % 4];
+	e1.type = EnemyType(rand() % 3);
+
+	return e1;
+}
+
+int getEnemyTypeString(EnemyType e1) //ejercicio 5
+{
+	string boi;
+}
