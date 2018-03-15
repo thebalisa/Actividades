fd2d60130885f25f45835e4ee100a518802aaf1d
Lluís Rodríguez Mateu <1999lluis@gmail.com>
2018-03-15 13:27:17 +0100

    3/15/2018

---
+++ AA1_05/Source.cpp
@@ -0,0 +1,8 @@
+#include "Vector.h"
+
+
+int main()
+{
+	addVectors;
+	return 0;
+}
--- AA1_05/Vector.h
+++ AA1_05/Vector.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 struct personas
 {
@@ -10,4 +11,4 @@ struct personas
 };
 
 
-std::vector<int>v1(5);std::vector<int>v2(10);std::vector<int>v3({ 10,9,8,7,6,5,4,3,2,1 });std::vector<std::string>v4({ "Gertrudis","Pancracia","Anacleto","Hipólito","Eustaquio","Fulgencia" });std::vector<int>v5(v3);std::vector<personas>v6({ { "Juanjo Chicote Jacas","25-03-1987","12345678L" } });//addVectorsvoid addVectors(std::vector<int>a, std::vector<int>b){	for (std::vector<int>::iterator it = a.begin(); it != a.end(); ++it)		std::}
+std::vector<int>v1(5);std::vector<int>v2(10);std::vector<int>v3({ 10,9,8,7,6,5,4,3,2,1 });std::vector<std::string>v4({ "Gertrudis","Pancracia","Anacleto","Hipólito","Eustaquio","Fulgencia" });std::vector<int>v5(v3);std::vector<personas>v6({ { "Juanjo Chicote Jacas","25-03-1987","12345678L" } });//addVectorsvoid addVectors(std::vector<int>a, std::vector<int>b){	std::transform(a.begin(), a.end(), b.begin(), a.begin(), std::plus<int>());}
